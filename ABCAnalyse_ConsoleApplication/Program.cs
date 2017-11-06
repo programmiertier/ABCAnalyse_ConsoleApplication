fbebb7315670164c288e6e69a4c518a8d4115b4c
programmiertier <zimmermannveronique@protonmail.com>
2017-11-06 15:15:48 +0100

    ABC in Yoda-SQL

--- ABCAnalyse_ConsoleApplication/Program.cs
+++ ABCAnalyse_ConsoleApplication/Program.cs
@@ -9,7 +9,7 @@ namespace ABCAnalyse_ConsoleApplication
 {
     class Program
     {
-        public static List<char> nennungen = new List<char>() { 'a', 'b', 'Z', 'L', 'q', 'C', 'M', 'b', 'Z', 'L', 'q', 'C', 'M', 'b', 'Z', 'L', 'q', 'C', 'M', 'b', 'Z', 'L', 'q', 'C', 'M' };    // 'a', 'b', 'Z', 'L', 'q', 'C', 'M'
+        public static List<char> nennungen = new List<char>() { 'a', 'b', 'Z', 'L', 'q', 'C', 'M', 'b', 'Z', 'L', 'q', 'C', 'M', 'b', 'F', 'L', 'q', 'C', 'M', 'g', 'Z', 'L', 'q', 'g', 'M' };    // 'a', 'b', 'Z', 'L', 'q', 'C', 'M'
 
 
         
@@ -21,12 +21,14 @@ namespace ABCAnalyse_ConsoleApplication
             // var query = from anzahl in nennungen group anzahl by anzahl into neueAnzahl select new { treffer = neueAnzahl.Count() }; // zählt Buchstaben
             // var query = from anzahl in nennungen group nennungen by nennungen into neueAnzahl select new { alle = neueAnzahl.Count() };    // zählt alle
             // var query = from anzahl in nennungen group anzahl by anzahl into neueAnzahl select new { treffer = neueAnzahl.Count(), wer = nennungen };   // Buchstabe wie oft
-            var query = from anzahl in nennungen group anzahl by anzahl into neueAnzahl
-                        select new { treffer = neueAnzahl.Count(), wer = neueAnzahl.First() }; // Buchstabe wie oft, welcher genau
+            var query = (from anzahl in nennungen
+                         group anzahl by anzahl into neueAnzahl
+                         orderby (neueAnzahl.Count()) descending
+                         select new { treffer = neueAnzahl.Count(), wer = neueAnzahl.First() }); // Buchstabe wie oft, welcher genau
             {
                 foreach ( var item in query )
                 {
-                    WriteLine("{0}, {1}", item.treffer, item.wer);
+                    WriteLine("{0}: {1}", item.treffer, item.wer);
                 }
             }
             ReadLine();
