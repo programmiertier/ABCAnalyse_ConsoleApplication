c313f39c55037c06206e6f549d84f142fb096486
programmiertier <zimmermannveronique@protonmail.com>
2017-11-06 15:03:23 +0100

    ABC mit Yoda Sql

--- ABCAnalyse_ConsoleApplication/Program.cs
+++ ABCAnalyse_ConsoleApplication/Program.cs
@@ -3,13 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
 
 namespace ABCAnalyse_ConsoleApplication
 {
     class Program
     {
+        public static List<char> nennungen = new List<char>() { 'a', 'b', 'Z', 'L', 'q', 'C', 'M', 'b', 'Z', 'L', 'q', 'C', 'M', 'b', 'Z', 'L', 'q', 'C', 'M', 'b', 'Z', 'L', 'q', 'C', 'M', 'a', 'b', 'Z', 'L', 'q', 'C', 'M', 'b' };    // 'a', 'b', 'Z', 'L', 'q', 'C', 'M'
+
+
+        
+
         static void Main(string[] args)
         {
+            WriteLine("Anzahl der Nennungen: {0}", nennungen.Count);
+
+            var query = from anzahl in nennungen group nennungen by nennungen into neueAnzahl select new { a = neueAnzahl.Count() };
+            {
+                foreach ( var item in query )
+                {
+                    WriteLine(item.a);
+                }
+            }
+            ReadLine();
         }
     }
 }
