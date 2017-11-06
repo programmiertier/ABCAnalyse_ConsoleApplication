55159cd523dc40b94053185ab5df27b2083a1e6c
programmiertier <zimmermannveronique@protonmail.com>
2017-11-06 15:11:16 +0100

    ABC

--- ABCAnalyse_ConsoleApplication/Program.cs
+++ ABCAnalyse_ConsoleApplication/Program.cs
@@ -9,7 +9,7 @@ namespace ABCAnalyse_ConsoleApplication
 {
     class Program
     {
-        public static List<char> nennungen = new List<char>() { 'a', 'b', 'Z', 'L', 'q', 'C', 'M', 'b', 'Z', 'L', 'q', 'C', 'M', 'b', 'Z', 'L', 'q', 'C', 'M', 'b', 'Z', 'L', 'q', 'C', 'M', 'a', 'b', 'Z', 'L', 'q', 'C', 'M', 'b' };    // 'a', 'b', 'Z', 'L', 'q', 'C', 'M'
+        public static List<char> nennungen = new List<char>() { 'a', 'b', 'Z', 'L', 'q', 'C', 'M', 'b', 'Z', 'L', 'q', 'C', 'M', 'b', 'Z', 'L', 'q', 'C', 'M', 'b', 'Z', 'L', 'q', 'C', 'M' };    // 'a', 'b', 'Z', 'L', 'q', 'C', 'M'
 
 
         
@@ -18,11 +18,15 @@ namespace ABCAnalyse_ConsoleApplication
         {
             WriteLine("Anzahl der Nennungen: {0}", nennungen.Count);
 
-            var query = from anzahl in nennungen group nennungen by nennungen into neueAnzahl select new { a = neueAnzahl.Count() };
+            // var query = from anzahl in nennungen group anzahl by anzahl into neueAnzahl select new { treffer = neueAnzahl.Count() }; // zählt Buchstaben
+            // var query = from anzahl in nennungen group nennungen by nennungen into neueAnzahl select new { alle = neueAnzahl.Count() };    // zählt alle
+            // var query = from anzahl in nennungen group anzahl by anzahl into neueAnzahl select new { treffer = neueAnzahl.Count(), wer = nennungen };   // Buchstabe wie oft
+            var query = from anzahl in nennungen group anzahl by anzahl into neueAnzahl
+                        select new { treffer = neueAnzahl.Count(), wer = neueAnzahl.First() }; // Buchstabe wie oft, welcher genau
             {
                 foreach ( var item in query )
                 {
-                    WriteLine(item.a);
+                    WriteLine("{0}, {1}", item.treffer, item.wer);
                 }
             }
             ReadLine();
